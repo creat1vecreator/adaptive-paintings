e5c912b1014b5701a826f703eef9b9850fb4ee6b
andrey <chistyakov000@mail.ru>
2022-07-19 01:59:03 +0300

    some conflict in logic

--- src/store/paintingsSlice.js
+++ src/store/paintingsSlice.js
@@ -1,16 +1,27 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { setCurrentPage, setTotalPages } from './pagesSlice';
+import { getAllPaintings } from '../requests/request';
 
 export const getPaintingsByFilters = createAsyncThunk(
   'paintings/getPaintingsByFilters',
-  async ({ url }, { dispatch }) => {
-    console.log('got url in paintings slice:', url);
-    const response = await fetch(url);
-    const data = await response.json();
-    dispatch(setCurrentPage(1));
-    dispatch(setTotalPages(Math.ceil(data.length / 12)));
+  // eslint-disable-next-line consistent-return
+  async ({ url }, { dispatch, getState }) => {
+    const { currentPage } = getState().pages;
+    const { totalPages } = getState().pages;
 
-    return data;
+    console.log('page in getPaintings by filter', currentPage);
+    console.log('got url in paintings slice:', url);
+    const newURL = new URL(url);
+    console.log('total pages in haha:', totalPages);
+    if (!totalPages) {
+      console.log('!totalPages', !totalPages);
+      newURL.searchParams.append('_limit', 12);
+      const response = await fetch(newURL);
+      const data = await response.json();
+      dispatch(setCurrentPage(1));
+      getAllPaintings().then((res) => dispatch(setTotalPages(Math.ceil(res.data.length / 12))));
+      return data;
+    }
   },
 );
 const paintingsSlice = createSlice({
